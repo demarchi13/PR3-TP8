77ba428ba9f8ddc3471db0b4bb0b19e3e480e398
Lucas De Marchi <131633835+demarchi13@users.noreply.github.com>
2024-06-07 16:26:17 -0300

    Update EliminarSucursal.aspx.cs

--- Vistas/EliminarSucursal.aspx.cs
+++ Vistas/EliminarSucursal.aspx.cs
@@ -22,12 +22,15 @@ namespace Vistas
 
         protected void btnEliminar_Click(object sender, EventArgs e)
         {
+            //PARTE 1
             Sucursal objSucursal = new Sucursal();
             objSucursal.IdSucursal1 = Convert.ToInt32(txtID.Text);
 
+            //PARTE 2
             if (objNegocio.EliminarSucursal(objSucursal))   lblMensaje.Text = "La sucursal se ha eliminado con exito";
+            
+            //PARTE 3
             else lblMensaje.Text = "La sucursal no existe o ya se elimino";
-
             txtID.Text = "";
         }
     }
